3643ac3b0d8715f257ff1dd92bcb4993a51423b5
Zach Sievers <zach.sievers@serrala.com>
2022-02-22 16:08:32 -0500

    update state / useEffect

--- navbar/pages/index.js
+++ navbar/pages/index.js
@@ -1,13 +1,5 @@
-import Link from "next/link";
-import Header from "../components/Header";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.scss";
+import Layout from "../components/Layout";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  return <Layout></Layout>;
 }
--- navbar/pages/projects/project-1.js
+++ navbar/pages/projects/project-1.js
@@ -4,8 +4,6 @@ export default function firstProject() {
   return (
     <>
       <h1>First Project</h1>
-
-      <Image src="/hero2wide.jpg" height={500} width={800} alt="Your Name" />
     </>
   );
 }
