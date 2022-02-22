4ee4f2f06cc3ae038cb843a90be05f08995f7770
Zach Sievers <zach.sievers@serrala.com>
2022-02-22 10:05:09 -0500

    state/functionality webhooks

--- navbar/components/Header.js
+++ navbar/components/Header.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
 import classes from "./Header.module.scss";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(true);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
   const menuToggleHandler = () => {
     setMenuOpen((p) => !p);
   };
